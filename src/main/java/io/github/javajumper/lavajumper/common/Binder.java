eab676c8befbb27deeb8c1b5085abfd5fc82365f
JavaJump3r <jumpergooog@gmail.com>
2023-03-15 09:39:11 +0200

    Added few binder functions, binder documentation

--- src/main/java/io/github/javajumper/lavajumper/common/Binder.java
+++ src/main/java/io/github/javajumper/lavajumper/common/Binder.java
@@ -9,9 +9,26 @@ import net.minecraft.client.util.InputUtil;
 import java.util.function.Consumer;
 
 public class Binder {
-    public static void addBind(String displayName, int defaultKey, Consumer<MinecraftClient> callback)
+    /**
+     * Adds bind without default key
+     * @param displayName
+     * @param category
+     * @param callback
+     */
+    public static void addBind(String displayName, String category, Consumer<MinecraftClient> callback){
+        addBind(displayName,category,-1,callback);
+    }
+
+    /**
+     * Adds bind
+     * @param displayName
+     * @param category
+     * @param defaultKey
+     * @param callback
+     */
+    public static void addBind(String displayName, String category, int defaultKey, Consumer<MinecraftClient> callback)
     {
-        var bind = new KeyBinding(displayName, InputUtil.Type.KEYSYM, defaultKey, "JavaJumper's things"); //GLFW.GLFW_KEY_DOWN
+        var bind = new KeyBinding(displayName, InputUtil.Type.KEYSYM, -1, category); //GLFW.GLFW_KEY_DOWN
         KeyBindingHelper.registerKeyBinding(bind);
         ClientTickEvents.END_CLIENT_TICK.register(client ->
         {
@@ -19,5 +36,17 @@ public class Binder {
                 callback.accept(client);
             }
         });
+    }
+
+    /**
+     * Adds bind under "LavaJumper" category
+     * @param displayName
+     * @param defaultKey
+     * @param callback
+     * @api
+     */
+    public static void addBind(String displayName, int defaultKey, Consumer<MinecraftClient> callback)
+    {
+        addBind(displayName,"LavaJumper",defaultKey,callback);
     }
 }
