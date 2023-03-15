d989978175b368b52660d6177ea16165159736ca
JavaJump3r <jumpergooog@gmail.com>
2023-03-15 11:07:35 +0200

    Added force initialized

--- src/main/java/io/github/javajumper/lavajumper/LavaJumper.java
+++ src/main/java/io/github/javajumper/lavajumper/LavaJumper.java
@@ -15,13 +15,15 @@ import org.slf4j.LoggerFactory;
 import java.util.Arrays;
 	@Environment(EnvType.CLIENT)
 	public class LavaJumper implements ClientModInitializer {
-		private static Logger DebugOutput = LoggerFactory.getLogger("JJHUD");
+		private static Logger DebugOutput = LoggerFactory.getLogger("LavaJumper");
 		public static String dynmapId = "jjdmap";
 		public static final String modid = "jjhud";
 		public static ConfigGenerator LavaJumperConfig;
-
+		private static boolean isInitialized = false;
 		@Override
 		public void onInitializeClient() {
+			if(isInitialized)
+				return;
 
 			var config = new ConfigGenerator("LavaJumper");
 			config.restoreConfig();
@@ -30,6 +32,18 @@ import java.util.Arrays;
 				new ConfigTestClass();
 
 			log("LavaJumper Initialized");
+		}
+		public void forceInit(){
+			log("Calling force LavaJumper initialization");
+			try{
+				onInitializeClient();
+			}
+			catch (Exception e){
+				throw e;
+			}
+			finally {
+				isInitialized=true;
+			}
 		}
 		public static void log(Object... objects) {
 			var string = "";
